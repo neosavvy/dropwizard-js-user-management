617d60dec8d59ba642865f4b83232221268f9b5c
Adam Parrish <aparrish@neosavvy.com>
2012-04-20 14:35:38 -0400

    initial project structure

---
+++ hbo-image-client/src/main/java/com/neosavvy/App.java
@@ -0,0 +1,13 @@
+package com.neosavvy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
