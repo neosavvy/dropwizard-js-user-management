4bbfa8d3ffa5c2fa6f54fdc9f4df4eae317c1fe4
Adam Parrish <aparrish@neosavvy.com>
2012-05-07 12:35:43 -0400

    Adding the ability to click new user, add a view component, then insert a new user row.

--- commons-user-client/src/main/resources/backbone/js/application.js
+++ commons-user-client/src/main/resources/backbone/js/application.js
@@ -1,11 +1,79 @@
 window.User = Backbone.Model.extend({
-    idAttribute: "firstName"
+    defaults:{
+        "id":null,
+        "firstName":"",
+        "lastName":"",
+        "email":""
+    }
 });
 
 window.UserCollection = Backbone.Collection.extend({
     model: User
     ,url: 'http://local.commons-user.com/backend/user'
 });
+
+window.UserView = Backbone.View.extend({
+
+    template:_.template($('#tpl-user-detail').html()),
+
+    initialize:function() {
+        this.model.bind("change", this.render, this);
+    },
+
+    events:{
+        "click .save":"saveUser"
+    },
+
+    saveUser:function () {
+        this.model.set({
+            firstName:$('#firstName').val(),
+            lastName:$('#lastName').val(),
+            email:$('#email').val()
+        });
+        if (this.model.isNew()) {
+            app.userList.create(this.model);
+        } else {
+            this.model.save();
+        }
+        return false;
+    },
+
+    render:function (eventName) {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    },
+
+    close:function () {
+        $(this.el).unbind();
+        $(this.el).empty();
+    }
+})
+
+window.UserHeaderView = Backbone.View.extend({
+
+    template:_.template($('#tpl-header').html()),
+
+    initialize: function() {
+        this.render()
+    },
+
+    render: function( eventName ) {
+        $(this.el).html(this.template());
+        return this;
+    },
+
+    events:{
+        "click .new":"newUser"
+    },
+
+    newUser:function(event) {
+        if( app.userView ) app.userView.close();
+        app.userView = new UserView({model: new User()});
+        $('#userDetail').html(app.userView.render().el);
+        return false;
+    }
+
+})
 
 window.UserListView = Backbone.View.extend({
 
@@ -44,7 +112,12 @@ window.UserListItemView = Backbone.View.extend({
 var AppRouter = Backbone.Router.extend({
 
     routes:{
-        "":"list"
+        "":"list",
+        "users/:id":"userDetails"
+    },
+
+    initialize: function() {
+        $('#userHeader').html(new UserHeaderView().render().el);
     },
 
     list:function () {
@@ -52,6 +125,13 @@ var AppRouter = Backbone.Router.extend({
         this.userListView = new UserListView({model:this.userList});
         this.userList.fetch();
         $('#userList').html(this.userListView.render().el);
+    },
+
+    userDetails:function(id) {
+        this.user = this.userList.get(id);
+        if( app.userView ) app.userView.close();
+        this.userView = new UserView({model:this.user});
+        $('#userDetail').html(this.userView.render().el);
     }
 
 });
