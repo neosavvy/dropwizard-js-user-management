006c4c2f22e0f3e3f70be1bf45b018b3c6e71488
Adam Parrish <aparrish@neosavvy.com>
2012-05-07 09:51:48 -0400

    Adding a run and debug script to start the server in debug mode and suspend mode.

---
+++ debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=4000  -jar commons-user-service/target/commons-user-service-1.0-SNAPSHOT.jar server commons-user-service/src/main/resources/user-service.yml
